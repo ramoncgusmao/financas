8a9f3556b6bfcabe67f93ca899449cf385991bbb
ramoncgusmao <ramoncgusmao@gmail.com>
2023-01-29 12:16:12 -0300

    video 11 - rotas de extrado e consolidado por mes

--- controller/relatorio_controller.py
+++ controller/relatorio_controller.py
@@ -9,3 +9,12 @@ relatorio_service = RelatorioService()
 @relatorio_route.get("/saldo")
 def saldo(subtrair: str = 'saida'):
     return relatorio_service.saldo(subtrair)
+
+@relatorio_route.get("/extrato")
+def extrato(mes: int = 0, ano: int = 0): 
+    return relatorio_service.extrato(mes, ano)
+
+@relatorio_route.get("/consolidado/{tipo}/ano/{ano}")
+def consolidado_por_tipo(tipo: str, ano: str): 
+    ano = int(ano)
+    return relatorio_service.consolidado_por_tipo(tipo, ano)
--- service/relatorio_service.py
+++ service/relatorio_service.py
@@ -1,4 +1,6 @@
 
+from datetime import datetime
+from typing import List
 from service.lancamento_service import LancamentoService
 
 
@@ -26,5 +28,27 @@ class RelatorioService:
         return naturezas
 
     
+    def extrato(self, mes: int, ano: int):
+        data_atal = datetime.now()
+        if(mes == 0):
+            mes = data_atal.month
+        if(ano == 0):
+            ano = data_atal.year
 
+        lancamentos = self.lancamento_service.buscar_todos()
+        return list(filter(lambda lanc: lanc.mes == mes and lanc.ano == ano, lancamentos))
+    
+    def consolidado_por_tipo(self, tipo: str, ano: int):
+        lancamentos = self.lancamento_service.buscar_todos()
+        lancamentos_filter = list(filter(lambda lanc: lanc.tipo == tipo and lanc.ano == ano, lancamentos))
+        meses_dict = {}
 
+        for lancamento in lancamentos_filter:
+            if meses_dict.setdefault(lancamento.mes):
+                meses_dict[lancamento.mes] += lancamento.valor
+            else:
+                meses_dict[lancamento.mes] = lancamento.valor
+        return [
+            {"mes": mes, "valor": round(valor,2), "tipo": tipo}
+            for mes, valor in meses_dict.items()
+        ]
